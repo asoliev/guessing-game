f8b83b105406a14a6b7e53136efc79b2b5c226bc
asoliev <91322310+asoliev@users.noreply.github.com>
2022-08-08 00:54:51 +0500

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round(this.max);
     }
 
     lower() {
-
+        this.max = this.max - (this.max - this.min) / 2;
     }
 
     greater() {
-
+        this.min = this.min + (this.max - this.min) / 2;
     }
 }
 
